f57e61f69b13afdf8ff259dd57fe231057574934
Hans Reichenbach <HansReich25@gmail.com>
2012-09-30 17:49:44 -0400

    add the files made during Lab0

---
+++ Lab0/goodbye.s
@@ -0,0 +1,41 @@
+	.file	"hello.c"
+	.section	.rodata
+	.align	2
+.LC0:
+	.ascii	"Hello World!\000"
+	.text
+	.align	2
+.LC1:
+    .ascii "Goodbye World!\000"
+    .text
+    .align  2
+	.global	main
+	.type	main, %function
+main:
+	@ Function supports interworking.
+	@ args = 0, pretend = 0, frame = 0
+	@ frame_needed = 1, uses_anonymous_args = 0
+	mov	ip, sp
+	
+    stmfd	sp!, {fp, ip, lr, pc}
+	    sub	fp, ip, #4
+	
+        ldr	r0, .L3
+	    bl	puts
+    
+        ldr r0, .L3+4
+        bl puts
+
+	    mov	r3, #42
+	    mov	r0, r3
+
+	    sub	sp, fp, #12
+	ldmfd	sp, {fp, sp, lr}
+	bx	lr
+.L4:
+	.align	2
+.L3:
+	.word	.LC0
+	.word   .LC1
+    .size	main, .-main
+	.ident	"GCC: (GNU) 4.1.2"
