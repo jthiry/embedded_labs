9cb25723b417ebff34b471f9c5ce603c35801a3b
Hans Reichenbach <HansReich25@gmail.com>
2012-10-09 18:50:31 -0400

    fix the comments so that code compiles

--- lab1/part1-dev/part1-strTable.s
+++ lab1/part1-dev/part1-strTable.s
@@ -6,28 +6,28 @@
 strTable:
     @ args = 0, pretend = 0, frame = 0
     @ frame_needed = 0, uses_anonymous_args = 0
-    stmfd   sp!, {r4, r5}   ; args r0=&src, r1=&dest, r2=srcLen, r3=destLen store r4-r5
-    ; r0 = &src
-    ; r1 = &dst
-    ; r2 = srcLen
-    ; r3 = dstLen
-    ; r4 = cnt
-    ; r5 = x
+    stmfd   sp!, {r4, r5}   @ args r0=&src, r1=&dest, r2=srcLen, r3=destLen store r4-r5
+    @ r0 = &src
+    @ r1 = &dst
+    @ r2 = srcLen
+    @ r3 = dstLen
+    @ r4 = cnt
+    @ r5 = x
 
-    mov r4, #0            ; cnt = 0
-    cmp r4, r3            ; if(destLen <= 0)
-    bge .L11              ; Then Exit Function, else continue
-.L9:                      ; Main Loop
-    ldrb r5, [r1, r4] @ zero_extendqisi2    ; x = dest[cnt]
-.L7:                      ; Inner Loop
-    subs ip, ip, #23      ; ip -= 23
-    bpl .L7               ; Inner Loop until ip is negative
-    add ip, ip, #23       ; ip is now 0-23
-    cmp ip, r2            ; if (ip > srcLen)
-    strleb  r5, [r0, ip]  ; then src[ip] = x
-    add r4, r4, #1        ; increment counter
-    cmp r4, r3            ; if ( cnt < destLen)
-    blt .L9               ; Main Loop
+    mov r4, #0            @ cnt = 0
+    cmp r4, r3            @ if(destLen <= 0)
+    bge .L11              @ Then Exit Function, else continue
+.L9:                      @ Main Loop
+    ldrb r5, [r1, r4] @ zero_extendqisi2    @ x = dest[cnt]
+.L7:                      @ Inner Loop
+    subs ip, ip, #23      @ ip -= 23
+    bpl .L7               @ Inner Loop until ip is negative
+    add ip, ip, #23       @ ip is now 0-23
+    cmp ip, r2            @ if (ip > srcLen)
+    strleb  r5, [r0, ip]  @ then src[ip] = x
+    add r4, r4, #1        @ increment counter
+    cmp r4, r3            @ if ( cnt < destLen)
+    blt .L9               @ Main Loop
 .L11:
     ldmfd   sp!, {r4, r5}
     mov pc, lr
