02504cedea37c903945bfafb99d70c5620a884ef
Joseph Battaglia <jabat@Josephs-MacBook-Pro.local>
2012-10-09 21:50:08 -0400

    COMMENTED

--- lab1/part1-dev/part1-strTable.s
+++ lab1/part1-dev/part1-strTable.s
@@ -1,3 +1,5 @@
+    @ Group Number 15
+    @ Authors: Joseph Battaglia, Hans Reichenbach, Josh Thiry
     .file   "part1-strTable.c"
     .text
     .align  2
@@ -6,7 +8,7 @@
 strTable:
     @ args = 0, pretend = 0, frame = 0
     @ frame_needed = 0, uses_anonymous_args = 0
-    stmfd   sp!, {r4, r5} 
+    stmfd   sp!, {r4, r5} @ r6 is no longer needed
     @Args:
     @ r0 = &src
     @ r1 = &dst
@@ -32,13 +34,20 @@ strTable:
     @because our branch ends with ip negative, we must add 23 to get our final result:
     @ ip = x % 23
 
+
+	cmp	ip, r2 @ if (ip > srcLen)
+    
     @two loads were removed: 
     @   one was a repeat load from dest[cnt] into x
     @   the second one was only used to compare the value, and NOT store if they were equal
     @ instead of loading and comparing, we just always store the byte into src, the result is the same because equal values are just overridden 
+    @ This allows us to remove a branch instruction
 
-	cmp	ip, r2 @ if (ip > srcLen)
     strleb  r5, [r0, ip]    @ then src[ip] = x
+    @Also, because there is now only one instruction in this part of the code (the store) we can make it conditional on the compare, thereby removing another branch
+    
+    @These last few lines are left the same, although further optimization would be to reverse the loop to count DOWN instead up up, which would save you the cmp instruction by doing a subs
+    @Note: doing this would also allow you to use the dstLen register as the counting register, freeing up a register so you only need to push/pop r4 onto the stack
 
     add r4, r4, #1        @ increment counter
     cmp r4, r3            @ if ( cnt < destLen)
