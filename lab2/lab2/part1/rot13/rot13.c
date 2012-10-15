8d4f471e2079ea90ae2151ea1b7ef68dbfb4516a
Joshua <jthiry@ubuntu.(none)>
2012-10-15 12:54:16 -0700

    add most comments

--- lab2/lab2/part1/rot13/rot13.c
+++ lab2/lab2/part1/rot13/rot13.c
@@ -1,62 +1,72 @@
 /*
  * rot13.c: ROT13 cipher test application
  *
- * Authors: Group Member 1 <email address>
- *          Group Member 2 <email address>
- * Date:    The current time & date
+ * Authors: Joe Battaglia <jabat295@gmail.com>
+ *          Hans Reichenback <HansReich25@gmail.com>
+ *	    Josh Thiry <josh.thiry@gmail.com>
+ * Date:    10/14/2012
  */
 #include <stdlib.h>
 #include <errno.h>
 
+//function to do the rot13 rotation
 char rotate(char);
-int read( int d, void *buff, size_t s);
-int write( int d, const void *buff, size_t s);
 
 int main(void) {
 
-	char my_buff[128];
+	
+	char my_buff[128];		//buffer to hold the input string
 	int num_chars, i, chk;
 	
+	//indefinite loop
 	while( 1 ) 
 	{
-		num_chars = read(0, my_buff, 128);
+		
+		num_chars = read(0, my_buff, 128);	//calling the read SWI to read in input
 		//printf("nchar=%d\n",num_chars); 
-		if(num_chars == 0) exit(0);
-		if(num_chars < 0) exit(1);
+		if(num_chars == 0) exit(0);		//if return length is 0, nothing was entered
+		if(num_chars < 0) exit(1);		//if return length < 0, error
 
+		//loop through the whole input str (minus the last char)
 		for(i = 0; i < num_chars - 1; i++)
 		{
 			//printf("%d\n", i ) ;
-			my_buff[i] = rotate(my_buff[i]);
+			my_buff[i] = rotate(my_buff[i]);	//call the rot14 function for current char
 		}
 
-		chk = write(1, my_buff, num_chars); 
+		chk = write(1, my_buff, num_chars); 	//calling write SWI to output newly rot13 str
+		
+		//if return value < 0, error
 		if (chk < 0 )
 		{
 			//printf("%d\n", errno);			
 			exit(1);
 		}
-		//done?
+		
 	}	
-	return -255;
+	return 0;	//program will never get to here
 }
 char rotate(char rot_me)
 {
-	int x = (int) rot_me;
-	if( (x < 65) || (x > 122) ) exit(1);
-	if( (x < 97) && (x > 90) ) exit(1);
+
+	int x = (int) rot_me;			//getting ascii value of char
+	if( (x < 65) || (x > 122) ) exit(1);	//value out of range, error
+	if( (x < 97) && (x > 90) ) exit(1);	//value out of range, error
 	//in bounds char
 
+	//char is A-M
 	if( x < 78 )
-		rot_me += 13;
+		rot_me += 13;	//add 13 to it
 	else
 	{
+		//char is either N-Z or n-z
 		if( x < 97 || x > 109)
-			rot_me -= 13;
+			rot_me -= 13;	//subtract 13
+		//else char is a-m
 		else
-			rot_me += 13;
+			rot_me += 13;	//add 13
 	}
 
-	return rot_me;
+	return rot_me;	//return the newly rotated char
 }
  
