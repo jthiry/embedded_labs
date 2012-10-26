e7de89c244db3afe164329bafb90cd068e2945a6
Hans Reichenbach <HansReich25@gmail.com>
2012-10-26 16:38:36 -0400

    work on hello.c for testing the kernel thoroughly

--- lab2/lab2/part2/hello/hello.c
+++ lab2/lab2/part2/hello/hello.c
@@ -1,8 +1,10 @@
 /*
- * hello.c: Hello world! with U-Boot Exports API
+ * hello.c: testbed file for making sure our kernel works right
  *
- * Author: Mike Kasick <mkasick@andrew.cmu.edu>
- * Date:   Sat, 13 Oct 2007 23:51:13 -0400
+ * Author: Joe Battaglia <JABAT295.gmail.com>
+ *         Hans Reichenbach <HansReich.gmail.com>
+ *         Josh Thiry <josh.thiry@gmail.com>
+ * Date:   10/26/2012
  */
 
 #include <exports.h>
@@ -12,16 +14,31 @@ int main(int argc, char* argv[]) {
 	int i;
 	char* str = "printing out args\n"
 	char my_buff[128];		//buffer to hold the input string
+	int errs = 0;
 
-	write(1, str, 19);
+  //args testing, input 3 words in command line
+	errs = write(1, str, 19);
+	printf("args write error: %x", errs);
 	for(i=0; i < argc; i++)
 	{
-		write(1, argv[i], 128);
+		errs = write(1, argv[i], 128);
+		printf("write loop[%d] error: %x", i, errs);
 	}
 
-	read(0, my_buff, 128); 
+  //check ?
+	errs = read(0, my_buff, 128);
+	printf("read  error: %x", errs);
 
-	write(1, my_buff, 19);
+	errs = write(1, my_buff, 19);
+	printf("write 3 error: %x", errs);
+
+  //check fd checks
+	errs = read(7, my_buff, 19);
+	printf("fd read error: %x", errs);
+	errs = write(8, my_buff, 19);
+	printf("fd write error: %x", errs);
+
+
 	/*things to check
     	buffer bounds
 		buffer+size bounds
