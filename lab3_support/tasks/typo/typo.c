334c20e23b64978a50c2549ce179074e140e185b
Hans Reichenbach <HansReich25@gmail.com>
2012-11-06 12:02:45 -0500

    first shot at typo done

--- lab3_support/tasks/typo/typo.c
+++ lab3_support/tasks/typo/typo.c
@@ -2,11 +2,52 @@
  *
  * @brief Echos characters back with timing data.
  *
+ * @author Joe Battaglia <JABAT295.gmail.com>
+ *         Hans Reichenbach <HansReich.gmail.com>
+ *         Josh Thiry <josh.thiry@gmail.com>
+ *
+ * @date 11/6/2012
+ *
  * Links to libc.
  */
 
+#include "typo.h"
+
 int main(int argc, char** argv)
 {
-	/* Add code here -- put your names at the top. */
+  //declare necessary variables
+  unsigned long start_time, end_time;
+  char read_buf[60];
+  float time_dif = 0.0;
+  int err_check = 0;
+
+  while(1) {
+    //print out the user prompt
+    putchar('~');
+    putchar(' ');
+
+    //get the current time
+    start_time = time();
+
+    //call read to let the user type in the prompt
+    //make sure to provide a sensical max length into read
+    err_check = read(STDIN_FILENO, read_buf, (size_t) 60);
+
+    //check that read didn't crash
+    if(err_check < 0) {
+      return -1;
+    }
+
+    //get the current time for comparison
+    end_time = time();
+
+    //echo what the user typed
+    printf("%s\n", read_buf);
+
+    //print how long it took them to type to tenth of second
+    time_dif = (end_time - start_time)*.001;
+    printf("You took %.1fs\n", time_dif);
+  }
+
 	return 0;
 }
---
+++ lab3_support/tasks/typo/typo.h
@@ -0,0 +1,24 @@
+/*
+ * @file typo.h
+ *
+ * @brief general header file for typo user program
+ *
+ * @author: Joe Battaglia <JABAT295.gmail.com>
+ *         Hans Reichenbach <HansReich.gmail.com>
+ *         Josh Thiry <josh.thiry@gmail.com>
+ *
+ * @date   11/6/2012
+ */
+
+#ifndef TYPO_H
+#define TYPO_H
+
+//necessary libs
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+
+//prototypes
+
+
+#endif
