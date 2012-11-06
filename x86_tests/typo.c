c419198a3e556a67cd6757776fa94d2eeddebac1
Hans Reichenbach <HansReich25@gmail.com>
2012-11-06 13:58:51 -0500

    got the function mostly working in linux environment instead of our kernel. It should work better in our kernel due to some weird ways linux handles time

--- x86_tests/typo.c
+++ x86_tests/typo.c
@@ -14,42 +14,41 @@
 //necessary libs
 #include <string.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <time.h>
 
 int main(int argc, char** argv)
 {
   //declare necessary variables
-  unsigned long start_time, end_time;
+  time_t start_time, end_time;
   char read_buf[60];
-  float time_dif = 0.0;
+  double time_dif = 0.0;
   int err_check = 0;
 
   while(1) {
     //print out the user prompt
-    putchar('~');
-    putchar(' ');
+    putc('~', stdout);
+    putc(' ', stdout);
 
     //get the current time
-    start_time = time();
+    start_time = time(NULL);
+
+    printf("start_time: %llu\n", (unsigned long long) start_time);
 
     //call read to let the user type in the prompt
     //make sure to provide a sensical max length into read
-    err_check = read(STDIN_FILENO, read_buf, (size_t) 60);
-
-    //check that read didn't crash
-    if(err_check < 0) {
-      return -1;
-    }
+    fgets(read_buf, sizeof(read_buf), stdin);
 
     //get the current time for comparison
-    end_time = time();
+    end_time = time(NULL);
+
+    printf("end_time: %llu\n", (unsigned long long) end_time);
 
     //echo what the user typed
     printf("%s\n", read_buf);
 
     //print how long it took them to type to tenth of second
-    time_dif = (end_time - start_time)*.001;
-    printf("You took %.1fs\n", time_dif);
+    time_dif = ((double)end_time - (double)start_time); ///CLOCKS_PER_SEC;
+    printf("You took %.1lfs\n", time_dif);
   }
 
 	return 0;
