02a5ad3bafceb186a186aff2302c650de76e9897
Hans Reichenbach <HansReich25@gmail.com>
2012-11-06 17:14:38 -0500

    turn off debug

--- lab3_support/kernel/main.c
+++ lab3_support/kernel/main.c
@@ -11,7 +11,7 @@
 #include <debug.h>
 
 uint32_t global_data;
-int debug_enabled = 1;
+int debug_enabled = 0;
 
 
 int kmain(int argc, char** argv, uint32_t table)
@@ -20,7 +20,7 @@ int kmain(int argc, char** argv, uint32_t table)
 
 	app_startup(); /* bss is valid after this point */
 	global_data = table;
-	
+
 	if(debug_enabled==1)puts("Wiring in abort handler...\n");
 
 	/* Wiring in Data abort handler */
@@ -29,25 +29,25 @@ int kmain(int argc, char** argv, uint32_t table)
 		    GET_EXP_VEC_ADDR(EX_FABRT));
 	    return 0xbadc0de;
 	}
-	
+
 	if(debug_enabled==1)puts("Setting up abort stack...\n");
 	/* setup abort stack */
 	setup_abort_stack();
 	setup_irq_stack();
 	//Our code starts here
-	
+
 	if(debug_enabled==1)puts("Wiring in swi handler...\n");
-	
+
 	if (-1 == wire_exception_handler(EX_SWI)) {
 	    printf ("ldr pc, [pc, #immed] not encountered at %d. Exiting.\n",
 		    GET_EXP_VEC_ADDR(EX_SWI));
 	    return 0xbadc0de;
 	}
-	
+
 
 	if(debug_enabled==1)puts("Wiring in irq handler...\n");
 
-	// Wiring in irq handler 
+	// Wiring in irq handler
 	if (-1 == wire_exception_handler(EX_IRQ)) {
 	    printf ("ldr pc, [pc, #immed] not encountered at %d. Exiting.\n",
 		    GET_EXP_VEC_ADDR(EX_FABRT));
@@ -55,7 +55,7 @@ int kmain(int argc, char** argv, uint32_t table)
 	}
 
 
-	
+
 	if(debug_enabled==1)puts("Initializing timers...\n");
 
 	initialize_timer();
@@ -71,7 +71,7 @@ int kmain(int argc, char** argv, uint32_t table)
 
 	if(debug_enabled==1)puts("Restoring handlers...\n");
 	restore_handlers();
-	
+
 	if(debug_enabled==1)puts("Exiting kernel.\n");
 
 	return status;
