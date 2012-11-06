220211018e0ef8c561898bb069cb98378a26d436
Hans Reichenbach <HansReich25@gmail.com>
2012-11-06 17:40:37 -0500

    revert hello back to what it's supposed to be

--- lab3_support/tasks/hello/hello.c
+++ lab3_support/tasks/hello/hello.c
@@ -13,11 +13,6 @@ int main(int argc, char** argv)
 {
 	const char hello[] = "Hello World\r\n";
 	write(STDOUT_FILENO, hello, sizeof(hello) - 1);
-	//while(1);
-
-	sleep(5000);
-
-	write(STDOUT_FILENO, hello, sizeof(hello) - 1);
 
 	return 0;
 }
