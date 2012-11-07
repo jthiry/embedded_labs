57bd84a2a9b773760748cac208868903d3e1fbcb
Josh Thiry <josh.thiry@gmail.com>
2012-11-07 15:01:56 -0500

    comments in splat and took out for loop

--- lab3_support/tasks/splat/splat.c
+++ lab3_support/tasks/splat/splat.c
@@ -13,32 +13,41 @@
 int main(int argc, char** argv)
 {
 
-	   const char upright_cursor[] = "|";
-	   const char fslash_cursor[] = "/";
-	   const char sideways_cursor[] = "-";
-	   const char bslash_cursor[] = "\\";
-	   const char backspace[] = "\b";
-	   unsigned long change_state = 200;
+	   const char upright_cursor[] = "|";		//upright bar
+	   const char fslash_cursor[] = "/";		//forward slanting bar
+	   const char sideways_cursor[] = "-";		//sideways bar
+	   const char bslash_cursor[] = "\\";		//backward slanting bar
+	   const char backspace[] = "\b";		//backspace to erase previous char
+	   unsigned long change_state = 200;		//rotate every 200ms
+	   int i;
 
-//	   while(1)
-    int i;
-    for(i = 0; i < 20; i++)
+    /*Continous loop that will print out a 
+    *rotating bar(cursor). Can't be terminated.*/	  
+    while(1)    
 	   {
+		//print out upright bar, then sleep for 200ms
 	   	write(STDOUT_FILENO, upright_cursor, 1);
 		sleep(change_state);
 
+		//erase the upright bar, and print out forward slanting bar
+		//sleep for 200ms
 		write(STDOUT_FILENO, backspace, 1);
 		write(STDOUT_FILENO, fslash_cursor, 1);
 		sleep(change_state);
 
+		//earase the forward slanting bar, and print out sideways bar
+		//sleep for 200ms
 		write(STDOUT_FILENO, backspace, 1);
 		write(STDOUT_FILENO, sideways_cursor, 1);
 		sleep(change_state);
 
+		//erase sideways bar, and print out backward slanting bar
+		//sleep 200ms
 		write(STDOUT_FILENO, backspace, 1);
 		write(STDOUT_FILENO, bslash_cursor, 1);
 		sleep(change_state);
 
+		//erase backward slanting bar to start loop again
 		write(STDOUT_FILENO,backspace, 1);
 
 	}
