0fd903cfc93743dbce3e48288c439aca345e3266
Hans Reichenbach <HansReich25@gmail.com>
2012-11-08 16:54:02 -0500

    fix typo

--- lab3_support/tasks/typo/typo.c
+++ lab3_support/tasks/typo/typo.c
@@ -38,6 +38,7 @@ int main(int argc, char** argv)
 	  //call read to let the user type in the prompt
 	  //make sure to provide a sensical max length into read
 	  err_check = read(STDIN_FILENO, read_buf, (size_t)MAX_INPUT_LEN);
+
 	  //check that read didn't crash
 	  if(err_check < 1) {
 		  return -1;
@@ -50,13 +51,11 @@ int main(int argc, char** argv)
 	  //echo what the user typed
 	  write(STDOUT_FILENO, read_buf, err_check);
 
-	  printf("%lu seconds\n", (diff/1000));
-	
 	  //print how long it took them to type to tenth of second
-	  //    time_dif = end_time - start_time;
-	  //    dif_ints = time_dif/1000;
-	  //  dif_dec = (time_dif/100) - (dif_ints*10);
-	  //    printf("You took %d.%d\n", dif_ints, dif_dec);
+	  time_dif = end_time - start_time;
+	  dif_ints = time_dif/1000;
+	  dif_dec = (time_dif/100) - (dif_ints*10);
+	  printf("You took %d.%d\n", dif_ints, dif_dec);
   }
 
   return 0;
