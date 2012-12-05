ff2d23f28d271c7d43ef3f5d1f902a1bd4a5c8ff
Joe Battaglia <jb295@buffalo.edu>
2012-12-05 04:52:15 -0500

    nothing

--- lab4/gdb_com.sh
+++ lab4/gdb_com.sh
@@ -1,4 +1,4 @@
 set architecture armv5te
 target remote localhost:1234
 add-symbol-file kernel/kernel 0xa3000000
-add-symbol-file tasks/bin/mutex_chaser 0xa0000000
+add-symbol-file tasks/bin/sys_err 0xa0000000
